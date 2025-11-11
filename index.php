2fd09bb6cefb91651fe12512c2f391ba8f845c30
Wiebe Gouma <goumawiebe1234@proton.me>
2025-11-11 20:10:56 +0100

    Design layout gedaan

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $Songs = array_diff(scandir($SongDir), array('.', '..'));
 $DirectoryIterator = 1;
 
 foreach ($Songs as $x) {
-    echo "$DirectoryIterator: $x\n";
+    // echo "$DirectoryIterator: $x\n";
     $DirectoryIterator++;
 }
 
@@ -15,12 +15,26 @@ foreach ($Songs as $x) {
 <html>
 
 <head>
-    <Title>Spotify Clone</Title>
+    <title>Spotify Clone</title>
     <link rel="stylesheet" href="./dist/output.css">
 </head>
 
 <body>
-    <p>Test</p>
+    <div class="flex flex-col h-screen">
+        <div id="TopBar" class="bg-gray-600 w-screen flex">
+            <h1 class="text-white align-middle">Top balk</h1>
+        </div>
+        <div class="flex grow">
+            <div id="SideBar" class="relative justify-center align-middle h-screen w-25 bg-gray-600 text-white">
+                <h1 class=" h-full">Hier playlists</h1>
+            </div>
+
+            <div id="MainContent" class="bg-gray-400 flex-1">
+                <p>Test</p>
+            </div>
+        </div>
+    </div>
+    <script src="./dist/index.js"></script>
 </body>
 
 </html>
